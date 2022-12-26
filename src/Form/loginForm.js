fd41bb652b8f096cfb8773f724507b3180a91918
yashpurkar <yashpurkar7079@gmail.com>
2022-12-26 21:11:15 +0530

    showed data using map method

--- src/Form/loginForm.js
+++ src/Form/loginForm.js
@@ -4,10 +4,22 @@ function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const [allData, setAllData] = useState([]);
+
+  function submitForm(e) {
+    e.preventDefault();
+
+    const newData = {
+      email: email,
+      password: password
+    }
+    // console.log(newData)
+    setAllData([...allData, newData]);
+  }
 
   return (
     <div>
-      <form action="">
+      <form onSubmit={submitForm}>
         <div>
           <label htmlFor="email">Email : </label>
           <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off" />
@@ -20,6 +32,19 @@ function LoginForm() {
 
         <button type="submit">Login</button>
       </form>
+
+      <div>
+        {
+          allData.map((currElem) => {
+
+            return (
+              <div>
+                <h3>{currElem.email} {currElem.password}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
