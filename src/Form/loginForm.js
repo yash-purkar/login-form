30b4615a4fff623bdb05361b836b9ddd81189086
yashpurkar <yashpurkar7079@gmail.com>
2022-12-26 20:57:57 +0530

    Done JSX

--- src/Form/loginForm.js
+++ src/Form/loginForm.js
@@ -1,6 +1,20 @@
 function loginForm() {
   return (
-    <h1>Login Form</h1>
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="email">Email : </label>
+          <input type="email" id="email" autoComplete="off" />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password : </label>
+          <input type="password" id="password" autoComplete="off" />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
